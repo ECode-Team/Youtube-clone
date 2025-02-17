223ba79b1c9efa8c6a5a3742010b267f8c05a2c7
yasinteymoori112 <yasinteymoori112@gmail.com>
2025-02-17 20:47:11 +0330

    Update Allowed hosts

--- Back end/YOUTUBE/settings.py
+++ Back end/YOUTUBE/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-h&ez)-#!ngb-yxv&j^2*(sb7*m%$i^=3%z&hqi6tn*8-mtfe70
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecopy369.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'ecopy369.pythonanywhere.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
