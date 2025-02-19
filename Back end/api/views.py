875ba297f5050ea54128720c42a7fd960589f52f
salar <salar_pr@outlook.com>
2025-02-19 21:42:26 +0330

    update API: GET, POST, PUT

--- Back end/api/views.py
+++ Back end/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import  Category,VIDEO
+from video.models import  Category,VIDEO
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.serializers import CategorySerializer,VideoSerializer,CreateVideoSerializer
@@ -7,11 +7,10 @@ from rest_framework import status
 
 # View for the home page
 @api_view(['GET','POST'])
-def api_video(request):
+def home_video(request):
   if request.method == "GET":
     data = VIDEO.objects.all()
     Video_Serializer =  VideoSerializer(data,many=True)
-
     return Response(Video_Serializer.data)
   if request.method == "POST":
     REQ_data  = request.data
@@ -20,3 +19,16 @@ def api_video(request):
     Serializer.seve()
     return Response(Serializer.data,status=201)
     
+@api_view(['PUT'])
+def update_video(request,id):
+  video_obj = VIDEO.objects.get(id=id)
+
+  Serializer = VideoSerializer(
+    isinstance=video_obj,
+    data=request.data,
+    )
+  Serializer.is_valid(raise_exception=True)
+
+  Serializer.save()
+
+  return Response(Serializer.data,status=200) 
