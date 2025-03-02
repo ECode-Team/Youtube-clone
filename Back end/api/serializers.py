40f97ad8766fa85b53ce3e2760708597dcd1e6b2
yasin0121 <ecopy369@gmail.com>
2025-03-02 18:12:04 +0000

    change Abbreviation os view and count_like

--- Back end/api/serializers.py
+++ Back end/api/serializers.py
@@ -2,13 +2,61 @@ from rest_framework import serializers, viewsets, permissions
 from django.contrib.auth.models import User
 from video.models import Category,VIDEO,Channel
 from account.models import Account,AccountManager
+from django.utils import timezone
+from datetime import timedelta
 
 class VideoSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField()
     uploaded_by = serializers.StringRelatedField()
+    views = serializers.SerializerMethodField()
+    count_like = serializers.SerializerMethodField()
+    time_since_uploaded = serializers.SerializerMethodField()
     class Meta:
         model = VIDEO
         fields = '__all__'
+
+    def get_views(self, obj):
+        return self.format_large_number(obj.views)
+
+
+    def get_count_like(self, obj):
+        return self.format_large_number(obj.count_like)
+
+
+    def format_large_number(self, number):
+        if number >= 1_000_000:
+            return f"{number / 1_000_000:.1f}M"
+        elif number >= 1_000:
+            return f"{number / 1_000:.1f}K"
+        else:
+            return str(number)
+
+    def get_time_since_uploaded(self, obj):
+        # محاسبه تفاوت زمانی بین زمان فعلی و زمان آپلود
+        now = timezone.now()
+        time_difference = now - obj.uploaded_at
+
+        # بررسی اختلاف زمانی
+        if time_difference < timedelta(minutes=1):
+            return f"{int(time_difference.total_seconds())} seconds"
+        elif time_difference < timedelta(hours=1):
+            minutes = int(time_difference.total_seconds() // 60)
+            return f"{minutes} minutes"
+        elif time_difference < timedelta(days=1):
+            hours = int(time_difference.total_seconds() // 3600)
+            return f"{hours} hours"
+        elif time_difference < timedelta(weeks=1):
+            days = int(time_difference.total_seconds() // 86400)
+            return f"{days} days"
+        elif time_difference < timedelta(weeks=4):
+            weeks = int(time_difference.total_seconds() // 604800)
+            return f"{weeks} weeks"
+        elif time_difference < timedelta(weeks=52):
+            months = int(time_difference.total_seconds() // 2592000)
+            return f"{months} months"
+        else:
+            years = int(time_difference.total_seconds() // 31536000)
+            return f"{years} years"
 
 class CategorySerializer(serializers.ModelSerializer):
     child_videos = VideoSerializer(many=True, read_only=True)
