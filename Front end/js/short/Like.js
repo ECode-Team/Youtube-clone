1bca4a33e43beb1b9c780d77a6d44e9889dc27b9
parham0121 <parysolh@gmail.com>
2025-03-10 15:40:39 +0330

    create like system

--- Front end/js/short/Like.js
+++ Front end/js/short/Like.js
@@ -1,3 +1,15 @@
-const cointainer = document.querySelector(".cointainer-down")
+const like = document.querySelector(".icon.like");
+const dislike = document.querySelector(".icon.dislike");
 
-function toggleReaction (cointainer) 
+function toggleLike(element, child, opposite, oppositeChild) {
+    return () => {
+        element.classList.toggle(child); // دکمه‌ی کلیک شده را تغییر وضعیت بده
+
+        if (opposite.classList.contains(oppositeChild)) {
+            opposite.classList.remove(oppositeChild); // دکمه‌ی مخالف را غیرفعال کن
+        }
+    };
+}
+
+like.addEventListener("click", toggleLike(like, "liked", dislike, "disliked"));
+dislike.addEventListener("click", toggleLike(dislike, "disliked", like, "liked"));
