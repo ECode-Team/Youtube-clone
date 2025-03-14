d48558b2c348691c02d6a5212f23d4e794f1fb22
parham0121 <parysolh@gmail.com>
2025-03-14 17:59:24 +0330

    Add share profile icons and implement close functionality for share box

--- Front end/js/playback-page/PB-share.js
+++ Front end/js/playback-page/PB-share.js
@@ -1,5 +1,6 @@
 const share = document.querySelector(".action.share");
 const boxShare = document.querySelector(".box-share");
+const closedButton = document.querySelector(".closed-icon")
 const shadowShare = document.querySelector(".cover-black");
 
   share.addEventListener("click", () => {
@@ -7,6 +8,11 @@ const shadowShare = document.querySelector(".cover-black");
     shadowShare.classList.add("active")
   });
 
+  closedButton.addEventListener("click", () => {
+    boxShare.classList.remove("active")
+    shadowShare.classList.remove("active")
+  });
+  
   shadowShare.addEventListener("click",  () => {
     boxShare.classList.remove("active")
     shadowShare.classList.remove("active")
